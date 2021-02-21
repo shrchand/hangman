2011544b67e9af0ac473b9834470f7967397a9fd
Shreeya Chand <76849512+shreeyachand@users.noreply.github.com>
2021-02-21 14:52:41 -0500

    more condensed

--- java-hangman-v2 (1)/Word.java
+++ java-hangman-v2 (1)/Word.java
@@ -1,33 +1,26 @@
-import java.util.Random;
-import java.util.Scanner;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.nio.file.*;
+import java.nio.charset.Charset;
 public class Word {
   String hangmanWord;
   void getEasyWord(){
     System.out.println("Getting your word...");
-    List<String> lines = Collections.emptyList();
+    List<String> lines = new ArrayList<>();
     try {
-      lines = Files.readAllLines(Paths.get("words.txt"), StandardCharsets.UTF_8);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+      lines = Files.readAllLines(Paths.get("words.txt"), Charset.defaultCharset());
+    } catch (IOException e) {}
     Random word = new Random();
     int randNum = word.nextInt(lines.size());
     hangmanWord = lines.get(randNum);
   }
   void getHardWord(){
     System.out.println("Getting your word...");
-    List<String> lines = Collections.emptyList();
+    List<String> lines = new ArrayList<>();
     try {
-      lines = Files.readAllLines(Paths.get("sowpods.txt"), StandardCharsets.UTF_8);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+      lines = Files.readAllLines(Paths.get("sowpods.txt"), Charset.defaultCharset());
+    } catch (IOException e) {}
     Random word = new Random();
     int randNum = word.nextInt(lines.size());
     hangmanWord = lines.get(randNum);
