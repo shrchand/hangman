c7dbc6d586dbca522fe495d2bdb0815bda0c3b91
Shreeya Chand <76849512+shreeyachand@users.noreply.github.com>
2021-04-16 08:12:23 -0400

    condense

--- java-hangman-v2 (1)/Word.java
+++ java-hangman-v2 (1)/Word.java
@@ -5,22 +5,18 @@ import java.nio.file.*;
 import java.nio.charset.Charset;
 public class Word {
   String hangmanWord;
-  void getEasyWord(){
+  void getEasyWord() throws IOException {
     System.out.println("Getting your word...");
     List<String> lines = new ArrayList<>();
-    try {
-      lines = Files.readAllLines(Paths.get("words.txt"), Charset.defaultCharset());
-    } catch (IOException e) {}
+    lines = Files.readAllLines(Paths.get("words.txt"), Charset.defaultCharset());
     Random word = new Random();
     int randNum = word.nextInt(lines.size());
     hangmanWord = lines.get(randNum);
   }
-  void getHardWord(){
+  void getHardWord() throws IOException {
     System.out.println("Getting your word...");
     List<String> lines = new ArrayList<>();
-    try {
-      lines = Files.readAllLines(Paths.get("sowpods.txt"), Charset.defaultCharset());
-    } catch (IOException e) {}
+    lines = Files.readAllLines(Paths.get("sowpods.txt"), Charset.defaultCharset());
     Random word = new Random();
     int randNum = word.nextInt(lines.size());
     hangmanWord = lines.get(randNum);
