7d8d8ffb2ffa3efa4514c709750468e13ac730ae
Shreeya Chand <76849512+shreeyachand@users.noreply.github.com>
2021-04-16 08:11:58 -0400

    condense

--- java-hangman-v2 (1)/Main.java
+++ java-hangman-v2 (1)/Main.java
@@ -1,6 +1,7 @@
 import java.util.*;
+import java.io.*;
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String alpha = "abcdefghijklmnopqrstuvwxyz";
     int strikes = 0;
     Scanner input = new Scanner(System.in);
